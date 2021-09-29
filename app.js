6d046474388483158f5e2d290480896f8841d1cd
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-09-29 12:01:41 -0700

    connected HTML to javascript added event listeners and formatted colors

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import functions and grab DOM elements
+  
+  const nameInput = document.getElementById('name');
+  const button = document.getElementById('generate');
+  const firstName = document.getElementById('first-name');
+  
 
-// initialize global state
+  // set event listeners 
+  button.addEventListener('click', ()=>{
+    let newName = nameInput.value;
+    firstName.textContent = newName;
+  });
 
-// set event listeners 
+  // what are we doing?
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  
+  // when the button is clicked, 
+  // get the value of the input
+  // replace the text in the nametag 
+  // with the value in the input field
+  // whats the event we're listening for? CLICK!
